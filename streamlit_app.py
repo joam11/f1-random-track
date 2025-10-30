51150bbaff2cc9e6497e8f088ca6b1b12d0827a5
joam11 <joammendoza11@gmail.com>
2025-10-30 15:18:32 -0400

    Update streamlit_app.py
    Changed code to be able to have right number of generations

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,19 +11,6 @@ tracks = ['Australia', 'China', 'Japan', 'Bahrain', 'Saudi Arabia', 'Miami',
           'Hungary', 'Zandvoort', 'Monza', 'Baku', 'Singapore', 'COTA', 'Mexico',
           'Brazil', 'Las Vegas', 'Qatar', 'Abu Dhabi']
 
-def track_selection(tracks, K):
-  selected_track_index = []
-  index = 0
-
-  while index < K:
-    seed = random.randint(1,24)
-    if seed not in selected_track_index:
-      selected_track_index.append(seed)
-      index += 1
-
-  return selected_track_index
-
-#my_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 st.subheader("\nSelect how many tracks you want to generate:")
 number = st.slider(
     label='Number of Races',
@@ -31,8 +18,8 @@ number = st.slider(
     max_value=len(tracks),
     value=1
 )
-indices = track_selection(tracks, number)
-st.subheader("\nHere is your track selection:")
-for i in indices:
-  st.write(tracks[i])
+
+st.subheader("Here is your track selection:")
+for t in selected_tracks:
+    st.write(f"🏁 {t}")
 
